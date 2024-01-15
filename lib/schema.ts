fb87db9a0ad7395a5dfff8c8a7a680bdf446da7f
Steve Meisner <steve.meisner@hey.com>
2024-01-15 17:50:43 -0500

    version bump

--- lib/schema.ts
+++ lib/schema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 export const ContactFormSchema = z.object({
-  name: z.string().min(1, { message: 'Name is required.' }),
+  name: z.string().min(1, { message: 'Name is required.' })
   email: z
     .string()
     .min(1, { message: 'Email is required.' })
